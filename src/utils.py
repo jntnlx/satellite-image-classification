8bedd20e488804e08faaf3e1cf8c2d1cd3220bec
Jonathan <212891216+jntnlx@users.noreply.github.com>
2026-06-15 23:38:32 +0200

    utility helper fct file formatting

--- src/utils.py
+++ src/utils.py
@@ -6,12 +6,11 @@ from torch.amp import autocast
 
 
 def seed_worker(worker_id):
-
     """
 
-        Seed workers for NumPy/random.
+    Seed workers for NumPy/random.
 
-        Called in PyTorch DataLoaders.
+    Called in PyTorch DataLoaders.
 
     """
 
@@ -22,42 +21,36 @@ def seed_worker(worker_id):
 
 # Custom augmentation
 def color_cast_approx(
-    
-    img: torch.Tensor, 
-    haze_intensity: float = 0.1, 
-    fixed_alpha: bool = False
-
+    img: torch.Tensor, haze_intensity: float = 0.1, fixed_alpha: bool = False
 ) -> torch.Tensor:
-
     """
 
-        Approximate blue hue color cast induced by atmospheric effects.
-        
-        Reference: figure 7 in EuroSAT paper.
-        Note: expected input is float32 tensor in [0, 1] range.
+    Approximate blue hue color cast induced by atmospheric effects.
+
+    Reference: figure 7 in EuroSAT paper.
+    Note: expected input is float32 tensor in [0, 1] range.
 
     """
 
     if img.dtype == torch.uint8:
-
         sample = img.to(torch.float32) / 255.0
 
     elif img.dtype == torch.float32:
-        
         sample = img
 
     else:
+        raise TypeError(
+            f"Unsupported input dtype: {img.dtype}. Expected torch.uint8 or torch.float32."
+        )
 
-        raise TypeError(f"Unsupported input dtype: {img.dtype}. Expected torch.uint8 or torch.float32.")
+    blue_tint = torch.tensor([0.6, 0.75, 1.0], device=img.device).view(
+        3, 1, 1
+    )  # alt: 0.5, 0.7, 1.0
 
-    blue_tint = torch.tensor([0.6, 0.75, 1.0], device=img.device).view(3, 1, 1)  # alt: 0.5, 0.7, 1.0
-
-    if fixed_alpha == True:
-
+    if fixed_alpha:
         alpha = haze_intensity
 
     else:
-
         alpha = torch.rand(1, device=img.device) * haze_intensity
 
     img_hazed = sample * (1 - alpha) + blue_tint * alpha
@@ -67,26 +60,24 @@ def color_cast_approx(
 
 # Augmentation upgrade
 def d4_tta(model, img):
-
     """
 
-        Test-Time Augmentation (TTA) helper function.
+    Test-Time Augmentation (TTA) helper function.
 
-        Apply D4 group symmetries: rotations and horiziontal flips.
-        Return averaged class logits.
+    Apply D4 group symmetries: rotations and horiziontal flips.
+    Return averaged class logits.
 
     """
 
     views = []
 
     for k in range(4):
-
         # 90-degree rotations and horizontally flipped versions
 
         # Rotation: 0, 90, 180, 270 degrees
         r = torch.rot90(img, k=k, dims=(2, 3))
         views.append(model(r))
-
+        
         # Horizontal flip
         f = torch.flip(r, dims=[3])
         views.append(model(f))
@@ -96,19 +87,11 @@ def d4_tta(model, img):
 
 
 # Model eval fct.
-def evaluate_model(
-    
-    model, 
-    dataloader, 
-    device, 
-    eval_approach='ref'
-    
-):
-
+def evaluate_model(model, dataloader, device, eval_approach="ref"):
     """
 
-        Validation/eval runner that supports:
-        Single-view ('ref') and multi-view Test-Time Augmentation ('tta').
+    Validation/eval runner that supports:
+    Single-view ('ref') and multi-view Test-Time Augmentation ('tta').
 
     """
 
@@ -117,7 +100,7 @@ def evaluate_model(
     all_lbls = []
     all_prds = []
 
-    use_cuda_amp = (device.type == 'cuda')
+    use_cuda_amp = device.type == "cuda"
 
     # Notes
     #   - `torch.inference_mode()`:
@@ -130,31 +113,26 @@ def evaluate_model(
 
     # with torch.no_grad():
     with torch.inference_mode():
-
         for img, lbl in dataloader:
-
             img = img.to(device, non_blocking=True)
             lbl = lbl.to(device, non_blocking=True)
 
             # Mixed Precision
             with autocast(
-
                 device_type=device.type,
                 dtype=torch.float16,
                 enabled=use_cuda_amp,
-                
             ):
-
                 # Reference (default)
-                if eval_approach == 'ref':  
-
+                if eval_approach == "ref":
                     # p = torch.softmax(model(img), dim=1)  # for probability-like outputs (e.g. confidence/calibration/uncertainty/...)
-                    p_ref = model(img)  # raw logits: sufficient for accuracy/precision/recall/F1/confusion matrix
+                    p_ref = model(
+                        img
+                    )  # raw logits: sufficient for accuracy/precision/recall/F1/confusion matrix
                     p = p_ref
 
                 # Test-Time Augementation (TTA)
-                elif eval_approach == 'tta':
-
+                elif eval_approach == "tta":
                     # Direct approach (old version)
                     # p1 = torch.softmax(model(img), dim=1)  # Standard
                     # p2 = torch.softmax(model(torch.flip(img, [3])), dim=1)  # Horizontal-Flip
@@ -167,7 +145,6 @@ def evaluate_model(
                     p = p_avg
 
                 else:
-
                     raise ValueError(f"Unknown eval_approach: {eval_approach}")
 
             prd = p.argmax(1)
